dfe3bef24255438f43b9c0356f95300fb12f9991
尚寂新 <shang.jixin@qq.com>
2020-07-22 14:52:14 +0800

    header.php add TDK and some meta tag

--- themes/AlphaPure/header.php
+++ themes/AlphaPure/header.php
@@ -5,6 +5,7 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 include('function.php');
 if ($pageType == 'index'){
+    //处理标题
     if (empty($_GET['dynamicsPage'])){
         $pageTitle = '我的动态 - '.$this->options->title;
     } else {
@@ -14,14 +15,20 @@ if ($pageType == 'index'){
             $pageTitle = '第'.intval($_GET['dynamicsPage']).'页 - 我的动态 - '.$this->options->title;
         }
     }
+    //处理描述
+    $pageDescription = $this->options->description;
 } elseif ($pageType == 'single') {
+    //处理标题
     if (strip_tags($this->dynamic->content) == ''){
         $pageTitle = '动态内容 - '.$this->options->title;
     } else {
         $pageTitle = mb_substr(strip_tags($this->dynamic->content), 0, 10, 'utf8').'... - 动态内容 - '.$this->options->title;
     }
+    //处理描述
+    $pageDescription = mb_substr(strip_tags($this->dynamic->content), 0, 200, 'utf8');
 } elseif ($pageType == '404'){
     $pageTitle = '页面没找到 - '.$this->options->title;
+    $pageDescription = '页面没找到：错误！该路径不存在！';
 }
 ?>
 <!DOCTYPE html>
@@ -31,7 +38,15 @@ if ($pageType == 'index'){
 <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1" />
 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
 <meta name="renderer" content="webkit" />
+<!-- DNS预加载 -->
+<meta http-equiv="x-dns-prefetch-control" content="on">
+<link rel="dns-prefetch" href="https://cdn.jsdelivr.net" />
+<meta content="telephone=no" name="format-detection" />
+<meta content="email=no" name="format-detection" />
 <title><?php echo $pageTitle; ?></title>
+<meta content="<?php echo $this->options->keywords; ?>" name="keywords" />
+<meta content="<?php echo $pageDescription; ?>" name="description" />
+<!-- 引用CSS -->
 <link rel="stylesheet" href="<?php Dynamics_Plugin::themeDirUrl("dynamic_index.css?version=1.3&time=2020.07.21.18") ?>"/>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
 </head>
