23b59736b073466c2182f914889938787307ba7c
SurGarfield <i@imxxz.cn>
2020-07-22 14:40:41 +0800

    SENMU Theme V1.0 For Nabo Dynamics
    SENMU Theme V1.0 For Nabo Dynamics
    
    Co-Authored-By: 森木志 <i@imxxz.cn>

--- themes/SENMU/index.php
+++ themes/SENMU/index.php
@@ -26,7 +26,7 @@ include('header.php');
 
 <?php while ($this->dynamics->next()) : ?>
     <div class="dynamic-content">
-<p><img src="<?php $this->dynamics->avatar(32) ?>" class="vimg" style="margin-right:10px;"><?php $this->dynamics->authorName();?><br><?php $this->dynamics->created() ?></p>
+<p><img src="<?php $this->dynamics->avatar(32) ?>" class="smzimg" style="margin-right:10px;"><?php $this->dynamics->authorName();?><br><?php $this->dynamics->created() ?></p>
 <div style=" margin:15px 5px 10px 55px;"><?php echo dynamicReplacer($this->dynamics->content); ?></div>
     </div>
 
