377f358bece479c890cfd3205d3bc535eb810400
陆之岇 <kraits@qq.com>
2021-07-20 19:06:59 +0800

    release 2.0.2

--- Plugin.php
+++ Plugin.php
@@ -4,7 +4,7 @@
  * 我的动态 - 南博助手
  * @package Dynamics
  * @author 陆之岇,尚寂新
- * @version 2.0.1
+ * @version 2.0.2
  * @link https://github.com/krait-team/Dynamics-typecho
  */
 class Dynamics_Plugin implements Typecho_Plugin_Interface
